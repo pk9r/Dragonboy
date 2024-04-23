753a8ed704d4e291e3ae6b6fdeeccf4c48dfa358
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-04-23 15:09:38 +0700

    Update CustomBackground.cs

--- GameProject/DragonBoy/Assets/Scripts/Assembly-CSharp/Mod/Graphics/CustomBackground.cs
+++ GameProject/DragonBoy/Assets/Scripts/Assembly-CSharp/Mod/Graphics/CustomBackground.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Threading;
 using UnityEngine;
 using Mod.R;
-#if UNITY_EDITOR || UNITY_STANDALONE
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE
 using SFB;
 #elif UNITY_ANDROID
 using EHVN;
