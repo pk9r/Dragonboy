8b88f5f3397a9d77363cf7f769aebe0cc83249b6
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-06-30 00:33:26 +0700

    Update AssemblyAttributes.cs

--- GameProject/DragonBoy/Assets/Scripts/Assembly-CSharp/Mod/AssemblyAttributes.cs
+++ GameProject/DragonBoy/Assets/Scripts/Assembly-CSharp/Mod/AssemblyAttributes.cs
@@ -2,4 +2,4 @@ using Unity.IL2CPP.CompilerServices;
 using UnityEngine.Scripting;
 
 [assembly: Preserve]
-[assembly: Il2CppSetOption(Option.DivideByZeroChecks, false)]
+[assembly: Il2CppSetOption(Option.DivideByZeroChecks, true)]
